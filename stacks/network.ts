f03261f74b1ead732857e8ea16c037bead46e524
Federico <federico.dayan@gmail.com>
2023-01-29 21:39:37 -0300

    Use existing VPC

--- stacks/network.ts
+++ stacks/network.ts
@@ -2,7 +2,8 @@ import { StackContext } from '@serverless-stack/resources';
 import { SecurityGroup, Vpc } from 'aws-cdk-lib/aws-ec2';
 
 export function Network({ stack, app }: StackContext) {
-  const vpc = new Vpc(stack, app.logicalPrefixedName('net'), { natGateways: 1 });
+  // const vpc = new Vpc(stack, app.logicalPrefixedName('net'), { natGateways: 1 });
+  const vpc = Vpc.fromLookup(stack, "vpc-08a1dceaa4a56c61b")
   const defaultLambdaSecurityGroup = new SecurityGroup(stack, 'DefaultLambda', {
     vpc,
     description: 'Default security group for lambda functions',
