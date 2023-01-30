bb5049504c682ff6fd30b133739f3ff0c2feca88
Federico <federico.dayan@gmail.com>
2023-01-30 12:04:22 -0300

    Setting vpcId

--- stacks/network.ts
+++ stacks/network.ts
@@ -3,7 +3,7 @@ import { SecurityGroup, Vpc } from 'aws-cdk-lib/aws-ec2';
 
 export function Network({ stack, app }: StackContext) {
   // const vpc = new Vpc(stack, app.logicalPrefixedName('net'), { natGateways: 1 });
-  const vpc = Vpc.fromLookup(stack, "vpc-08a1dceaa4a56c61b")
+  const vpc = Vpc.fromLookup(stack, "vpc-08a1dceaa4a56c61b", { vpcId: 'vpc-08a1dceaa4a56c61b' })
   const defaultLambdaSecurityGroup = new SecurityGroup(stack, 'DefaultLambda', {
     vpc,
     description: 'Default security group for lambda functions',
